93d4bc5924321231984939eaececa921c9380b7f
MrManHD <nikigalaxylp@gmail.com>
2024-03-17 13:21:07 +0100

    add github publish method in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,13 +20,19 @@ dependencies {
 }
 
 publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/avionik-world/fancy-kotlin-paper")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
     publications {
-        create<MavenPublication>(project.name) {
+        register<MavenPublication>("gpr") {
             from(components["java"])
-
-            groupId = project.group as String?
-            artifactId = project.name
-            version = project.version.toString()
         }
     }
 }
